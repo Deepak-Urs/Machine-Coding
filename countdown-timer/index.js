8e59b721247e9bbf350773bf47439e225ae5f083
Deepak-Urs <gvdeepakurs2202@gmail.com>
2023-10-21 15:09:57 -0700

    basic countdown timer app setup

---
+++ countdown-timer/index.js
@@ -0,0 +1,13 @@
+(function () {
+    var hour = document.querySelector('.hour')
+    var min = document.querySelector('.minute')
+    var sec = document.querySelector('.sec')
+
+    var startBtn = document.querySelector(".start");
+    var stopBtn = document.querySelector(".stop");
+    var resetBtn = document.querySelector(".reset");
+
+    //startBtn.addEventListener('click', () => {
+
+    //})
+})()
