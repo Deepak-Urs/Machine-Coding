a9a406c4ea2c0291447913a76ec3c01df5eb7a04
Deepak-Urs <gvdeepakurs2202@gmail.com>
2023-11-28 13:49:46 -0800

    added states for length and checkbox data

--- React questions/password-generator/src/App.js
+++ React questions/password-generator/src/App.js
@@ -1,12 +1,21 @@
+import { useState } from 'react';
 import './App.css';
 
 export default function App() {
-  const checkBoxData = [
+  const [length, setLength] = useState(4)
+  const [checkboxData, setCheckboxData] = useState([
     {title:'Include Uppercase Letters', state: false},
     {title:'Include Lowercase Letters', state: false},
     {title:'Include Numbers', state: false},
     {title:'Include Symbols', state: false},
-  ];
+  ])
+
+  const handleCheckboxChange = (i) => {
+    console.log('i seen', i);
+    const updatedCheckboxData = [...checkboxData]
+    updatedCheckboxData[i].state = !updatedCheckboxData[i].state
+    setCheckboxData(updatedCheckboxData)
+  }
 
   return (
     <div className='container'>
@@ -20,17 +29,17 @@ export default function App() {
       <div className='charlength'>
         <span>
           <label>Character length</label> 
-          <label>4</label>
+          <label>{length}</label>
         </span>
-        <input type='range' min='4' max='20'/>
+        <input type='range' min='4' max='20' onChange={(e) => setLength(e.target.value)} value={length}/>
       </div>
       
       {/* checkboxes */}
       <div className='checkboxes'>
-        {checkBoxData.map((checkbox, index) => {
+        {checkboxData.map((checkbox, index) => {
           return (
             <div>
-              <input key={index} type='checkbox' checked={checkbox.state}/>
+              <input key={index} type='checkbox' onChange={() => handleCheckboxChange(index)} checked={checkbox.state}/>
               <label>{checkbox.title}</label>
             </div>
           )
