aedb1b9d04c5620461296c9ba61d3e0e806dff06
Deepak-Urs <gvdeepakurs2202@gmail.com>
2024-02-12 15:56:26 -0800

    added modal UI

--- React questions/employee-management-system/src/App.js
+++ React questions/employee-management-system/src/App.js
@@ -7,6 +7,7 @@ import EmpCard from './components/EmpCard';
 function App() {
   const [empData, setEmpData] = useState([])
   const [selectedEmployee, setSelectedEmployee] = useState({})
+  const [toggleModal, setToggleModal] = useState(false)
 
   useEffect(() => {
     setEmpData(data)
@@ -18,13 +19,17 @@ function App() {
     setSelectedEmployee(selEmp)
   }
 
+  const hideModal = (e) => {
+    console.log(e);
+  }
+
   return (
     <div className="container">
 
       <div className='header'>
         <div className='header__title'>Employee DBMS</div>
         <div className='header__title'>
-          <button className='header__btn'>Add Employee</button>
+          <button className='header__btn' onClick={() => setToggleModal(!toggleModal)}>Add Employee</button>
         </div>
       </div>
 
@@ -39,6 +44,21 @@ function App() {
           <EmpCard info={selectedEmployee} />
         </div>
       </div>
+
+      {toggleModal && <div className='add-modal' onClick={(e) => hideModal(e)}>
+          <form className='add-Modal__form'>
+            <div className='add-Modal__form__header'>
+              <h1>Add New Employee</h1> 
+              <span style={{marginLeft: '10px', cursor: 'pointer'}} onClick={() => setToggleModal(!setToggleModal)}>❌</span>
+            </div>
+            <div><input type='text' name='firstName' placeholder='Enter firstName'/></div>
+            <div><input type='text' name='lastName' placeholder='Enter lastName'/></div>
+            <div><input type='text' name='salary' placeholder='Enter salary'/></div>
+            <div><input type='text' name='contactNumber' placeholder='Enter contactNumber'/></div>
+            <div><input type='text' name='dob' placeholder='Enter dob'/></div>
+            <div><input type='submit' /></div>
+          </form>
+      </div>}
     </div>
   );
 }
