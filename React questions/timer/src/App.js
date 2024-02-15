7fcc1cd8cdd3b7b9b9738599240b00b2486e9cb8
Deepak-Urs <gvdeepakurs2202@gmail.com>
2024-02-15 14:43:38 -0800

    update look and view of pomodoro timer

--- React questions/timer/src/App.js
+++ React questions/timer/src/App.js
@@ -6,6 +6,7 @@ const App = () => {
   const [min, setMin] = useState(0)
   const [sec, setSec] = useState(0)
   const [timer, setTimer] = useState()
+  const [showPauseBtn, setShowPauseBtn] = useState(false)
 
   useEffect(() => {
     // IMPORTANT NOTE: setSec(sec => sec-1) in startCounter is not directly updated in the state as an async fn is controlling the change, so we need to use the useEffect to forcefully update it.
@@ -13,6 +14,9 @@ const App = () => {
     // make code changes wrt edge cases here when sec == 0 and so on
     if (sec === 0 && min === 0 ){
       clearInterval(timer)
+      if(showPauseBtn) {
+        setShowPauseBtn(!showPauseBtn)
+      }
     }
     else if(sec === -1) {
       setMin(min => min - 1)
@@ -21,14 +25,15 @@ const App = () => {
   }, [min,sec, timer])
   
   const startCounter = () => {
+    setShowPauseBtn(!showPauseBtn)
     setTimer(setInterval(() => {
-      console.log('sec value', sec);
       setSec(sec => sec-1) 
       
     }, 1000))
   }
 
   const pauseCounter = () => {
+    setShowPauseBtn(!showPauseBtn)
     clearInterval(timer)
   }
 
@@ -40,22 +45,24 @@ const App = () => {
   
   return (
     <div className="App">
-      <h1>Timer</h1>
+      <h1>Pomodoro Timer</h1>
 
       <div className='counter'>
         <div className='min-div'>
+          <label>Minutes</label>
           <input type='number' name='min' value={min} onChange={(e) => setMin(e.target.value)}/>
         </div>
         <div className='colon'>
           :
         </div>
         <div className='sec-div'>
+        <label>Seconds</label>
           <input type='number' name='sec' value={sec} onChange={(e) => setSec(e.target.value)}/>
         </div>
       </div>
       <div className='actions'>
-        <div className='start btn' onClick={() => startCounter()}>Start</div>
-        <div className='pause btn' onClick={() => pauseCounter()}>Pause</div>
+        {!showPauseBtn && <div className='start btn' onClick={() => startCounter()}>Start</div>}
+        {showPauseBtn && <div className='pause btn' onClick={() => pauseCounter()}>Pause</div>}
         <div className='reset btn' onClick={() => resetCounter()}>Reset</div>
 
       </div>
