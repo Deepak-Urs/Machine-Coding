503ff979773bec2549beabfc3b9b7313870271ee
Deepak-Urs <gvdeepakurs2202@gmail.com>
2024-02-18 15:38:42 -0800

    updated the overall control to frontend and backend

--- React questions/pagination-component/src/App.js
+++ React questions/pagination-component/src/App.js
@@ -4,20 +4,27 @@ import './App.css';
 function App() {
   const [products, setProducts] = useState([])
   const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
 
   useEffect(() => {
     fetchProducts()
-  }, [])
+  }, [page])
 
   const fetchProducts = async() => {
-    const res = await fetch('https://dummyjson.com/products?limit=100');
+    const res = await fetch(`https://dummyjson.com/products?limit=10&skip=${page*10-10}`);
     const data = await res.json();
-    console.log(data);
-    setProducts(data.products)
+    if(data && data.products) {
+      console.log(data);
+      setProducts(data.products)
+      setTotalPages(data.total)
+    }
+
   }
 
   const selectedPageHandler = (selectedPage) => {
+    if(selectedPage >= 1 && selectedPage <= totalPages && selectedPage !== page) {
       setPage(selectedPage)
+    }
   }
 
   return (
@@ -25,7 +32,7 @@ function App() {
       {products.length > 0 && 
       <div className='products'>
         {
-          products.slice(page*10-10, page*10).map(prod =>(
+          products.map(prod =>(
           <span className='products__single' key={prod.id}>
             <img src={prod.thumbnail} alt={prod.title} />
             <span>{prod.title}</span>
@@ -34,10 +41,10 @@ function App() {
         </div>}
         {products.length > 0 && <div className='pagination'>
             {page > 1 && <span onClick={() => selectedPageHandler(page-1)}>◀</span>}
-            {[...Array(products.length/10)].map((_, i) => 
+            {[...Array(totalPages/10)].map((_, i) => 
               <span className={page === i+1 ? 'pagination_selected': ''} key={i} onClick={() => selectedPageHandler(i+1)}>{i+1}</span>
             )}
-            {page < (products.length/10) && <span onClick={() => selectedPageHandler(page+1)}>▶</span>}
+            {page < totalPages/10 && <span onClick={() => selectedPageHandler(page+1)}>▶</span>}
           </div>}
     </div>
   );
