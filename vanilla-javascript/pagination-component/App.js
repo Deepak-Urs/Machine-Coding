f15a8adb22f8e1f620f4e427ae97ae4b5f1ca3d6
Deepak-Urs <gvdeepakurs2202@gmail.com>
2024-02-21 14:23:22 -0800

    added paginator

--- vanilla-javascript/pagination-component/App.js
+++ vanilla-javascript/pagination-component/App.js
@@ -4,14 +4,52 @@
 //    //}
 //)()
 let products = []
+let page = 0
+let totalPages = 0
+let pages = []
 
-async function getProductsData() {
-    const res = await fetch('https://dummyjson.com/products?limit=100')
+let productsElement = document.querySelector('.products__list')
+let paginator = document.querySelector('.paginator');
+
+async function getProductsData(numProductsSkip) {
+
+    products = []
+    const res = await fetch(`https://dummyjson.com/products?limit=10&skip=${numProductsSkip}&select=title,price`)
     const data = await res.json()
     console.log(data);
     products = data.products
+    totalPages = (data.total)/10
 
-    let productsElement = document.querySelector('.products__list')
+    // setup of the paginator
+    
+    let pageId = document.createElement('div')
+    pageId.className = 'paginator-id'
+    pageId.innerHTML = '◀️'
+    pageId.style.border = '1px solid'
+    pageId.id = 0
+    paginator.appendChild(pageId)
+
+    for(let i=0; i < totalPages; i++) {
+        pages.push(i+1)
+        let pageId = document.createElement('div')
+        pageId.className = 'paginator-id'
+        pageId.style.border = '1px solid'
+        pageId.innerHTML = i+1
+        pageId.id = i
+        paginator.appendChild(pageId)
+    }
+
+    pageId = document.createElement('div')
+    pageId.className = 'paginator-id'
+    pageId.style.border = '1px solid'
+    pageId.innerHTML = '▶️'
+    pageId.id = 0
+    paginator.appendChild(pageId)
+
+    //numProductsSkip = numProductsSkip*10 - 10 
+    console.log('numProductsSkip', numProductsSkip);
+
+    
 
     for(let i =0; i< data.products.length; i++) {
         let productItem = document.createElement('div')
@@ -23,8 +61,6 @@ async function getProductsData() {
 
         let productItemImage = document.createElement('img')
         productItemImage.src = products[i].thumbnail
-        productItemImage.style.width = '150px'
-        productItemImage.style.height = '250px'
 
         productItem.appendChild(productItemTitle)
         productItem.appendChild(productItemImage)
@@ -37,4 +73,26 @@ async function getProductsData() {
 }
 
 
-getProductsData()
+getProductsData(0)
+
+paginator.addEventListener('click', (e) => {
+    pageNum = e.target.innerHTML
+    if (pageNum === '◀️') {
+        page = page - 1
+        numProductsSkip = ((page-1)*10 - 10).toString() 
+    }
+    else if (pageNum === '▶️') {
+        page = page + 1
+        numProductsSkip = ((page+1)*10 - 10).toString() 
+    }
+    else {
+        page = e.target.innerHTML
+        numProductsSkip = ((page)*10 - 10).toString() 
+    }
+    console.log('page', page);
+    console.log('numProductsSkip', numProductsSkip);
+   
+    getProductsData(numProductsSkip)
+    productsElement.innerHTML = ''
+    paginator.innerHTML = ''
+})
